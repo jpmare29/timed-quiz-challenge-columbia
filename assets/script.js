debc2a9c5f650abfa199f69c9a7d1e131be803fa
jpmare29 <jpmmare@yahoo.com>
2022-03-19 22:27:32 -0400

    ironed out bugs, finished logic, stored scores, and accesed high score to display

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+// retrieved html elements and declared global variables
+
 const timer = document.querySelector('.timer-value');
 const startButton = document.getElementById('start-btn');
 const nextButton = document.getElementById('next-btn');
@@ -5,7 +7,9 @@ const questionContainer = document.getElementById('question-container');
 const questionText = document.getElementById('question');
 const btn = document.getElementById('testBtn');
 const btnContainer = document.getElementById('answer-buttons');
-const highScoreDisplay = document.querySelector('.timer-value');
+const highScoreName = document.querySelector('.hs-name')
+const highScoreDisplay = document.querySelector('.hs-value');
+const playAgainButton = document.getElementById('play-again-btn');
 const questionArray = [{
     question: "Which is not a keyword to declare a variable?",
     answers: [
@@ -61,17 +65,29 @@ const questionArray = [{
     ]
 }]
 let highScore;
-let timeLeft = 11;
+let timeLeft = 30;
 let userScore = 0;
 let j = 0;
 
 function retrieveHighScore() {
-    const highScoreObj = localStorage.getItem('highScore');
-    highScore = highScoreObj.score;
-    highScoreDisplay.textContent = highScore;
+    // check localStorage for object that will be set once one play through has happened
+    // set it equal to a variable that will be manipulated
+    const highScoreObj = JSON.parse(localStorage.getItem('highScoreObj'));
+    // If there is no highScoreObj because the game hasn't been played in the browser
+    // the highscore will be initialized to No One 0 and displayed when the page loads
+    // since the function will be called first
+    if (!highScoreObj) {
+        highScore = 0;
+        highScoreDisplay.textContent = highScore;
+        highScoreName.textContent = 'No One'
+    } else {
+        highScore = highScoreObj[1];
+        highScoreDisplay.textContent = highScore;
+        highScoreName.textContent = highScoreObj[0];
+    }
+
 }
-
-
+// Timer Engine that will call the endGame function when there is no time left
 function countdown() {
 
 var timeInterval = setInterval(function() {
@@ -85,23 +101,26 @@ var timeInterval = setInterval(function() {
 }, 1000);
 
 }
-
+// function that will be called when the start button is pressed.
+// it hides the start button, shows the question container,
+// begins the countdown and calls the setNextQuestion function to set the first question.
 function startGame() {
     startButton.classList.add('hide');
     questionContainer.classList.remove('hide');
     countdown();
     setNextQuestion();
 }
-
+// This function will be called when the game starts and after the user selects an answer
+// as long as there is time remaining and questions in the array holding all of the questions.
 function setNextQuestion() {
     if (j === questionArray.length) {
-        endGame();
+        timeLeft = 0;
     }
-
+// clears previous answer buttons
     while(btnContainer.firstChild) {
         btnContainer.removeChild(btnContainer.firstChild);
     }
-
+// fills question text and populates container with new answer buttons
         questionText.textContent = questionArray[j].question;
     for (let i = 0; i < questionArray[j].answers.length; i++) {
         let tBtn = document.createElement('btn');
@@ -112,20 +131,39 @@ function setNextQuestion() {
     }
     j++;
 }
-    
+// function that is called when user click answer buttons and only answer buttons
+// if there are questions left in the array it will call the setNextQuestion function
+// else it will function as normal on the last question and empty the timer so that
+// the endGame function can be called without doing so twice
 function selectQuestion(event) {
         let element = event.target;
+        if (timeLeft > 0 && j < questionArray.length) {
         if (element.matches('.testBtn')) {
             if (element.id === 'true') {
                 userScore++;
-                console.log(userScore);
             } else {
                 timeLeft -= 3;
+                timer.textContent = timeLeft;
             }
             setNextQuestion();
         }
+    } else if (timeLeft > 0 && j <= questionArray.length) {
+        if (element.matches('.testBtn')) {
+            if (element.id === 'true') {
+                userScore++;
+            } else {
+                timeLeft -= 3;
+            }
+        }
+        if (j === questionArray.length) {
+            timeLeft = 0;
+            questionContainer.classList.add('hide');
+        }
+    }
 }
-
+// will prompt the user to enter initials that will be returned to be used in a later function
+// this function will accept numbers or special characters if the user wants
+// but will not allow an empty response.
 function getInitials() {
     let storeScore = window.prompt('Enter your initials:');
     if (!storeScore) {
@@ -134,32 +172,46 @@ function getInitials() {
     }
     return storeScore;
 }
-
+// function to initialize the timer, score, and question counter. It will also hide the 
+// play again button that shows up after the last question and displays the start button.
 function resetGame() {
-    timeLeft = 11;
+    timeLeft = 30;
     userScore = 0;
     j = 0;
+    playAgainButton.classList.add('hide');
+    startButton.classList.remove('hide');
 }
-
+// function to hide the last question, get initials from user, put them in local storage
+// and update the highScoreObj and the high score display on the page.
+// Displays the playAgain button which allows the user to restart the game.
 function endGame() {
-    let userInitials
-    //Hide questions
+    let userInitials;
     questionContainer.classList.add('hide');
 
-    //Get initials from user to create key for local storage
     if (userScore > highScore) {
         window.alert('New High Score!')
         userInitials = getInitials();
         highScore = userScore;
         highScoreDisplay.textContent = highScore;
-        localStorage.setItem('highScoreObj', JSON.stringify(userInitials), JSON.stringify(highScore));
+        highScoreName.textContent = userInitials;
+        localStorage.setItem('highScoreObj', JSON.stringify([userInitials, highScore]));
+        localStorage.setItem(JSON.stringify(userInitials), JSON.stringify(userScore));
     } else {
+        // it would be possible to create an object like the above highScoreObj to store
+        // all of the other scores. This could then be accesed to generate html elements
+        // containing all of the user scores by looping through the regularScoreObj but the 
+        // acceptance criteria did not state this as a necessary feature. Instead the regular scores
+        // are stored as Initials alongside Scores. The High Scores are stored this way too so they are
+        // not lost when a new high score is acheived.
         window.alert('Game Over')
         userInitials = getInitials();
         localStorage.setItem(JSON.stringify(userInitials), JSON.stringify(userScore));
     }
 
+    playAgainButton.classList.remove('hide');
 }
-
+// function call and Event listeners for button clicks
+retrieveHighScore();
 startButton.addEventListener('click', startGame);
 questionContainer.addEventListener('click', selectQuestion);
+playAgainButton.addEventListener('click', resetGame);
